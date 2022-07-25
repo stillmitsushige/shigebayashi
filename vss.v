2579bda3d81c16b60d955f0117053be23ed1c90f
zztkm <takumi.basket1682@gmail.com>
2022-07-26 01:55:59 +0900

    ✨ file を検索する機能の追加

--- vss.v
+++ vss.v
@@ -2,23 +2,37 @@ module main
 
 import os
 import cli
-
 import markdown
 
 fn main() {
-	mut app := cli.Command {
-		name: "vss"
-		version: "0.0.0"
-		description: "static site generator"
+	mut app := cli.Command{
+		name: 'vss'
+		version: '0.0.0'
+		description: 'static site generator'
 		execute: fn (cmd cli.Command) ? {
+			paths := get_paths("docs")
+			for path in paths {
+				println(path)
+			}
 			return
 		}
 	}
 
-    text := '# Markdown Rocks!'
-    output := markdown.to_html(text)
-    println(output) // <h1>Markdown Rocks!</h1>
+	text := '# Markdown Rocks!'
+	output := markdown.to_html(text)
+	println(output) // <h1>Markdown Rocks!</h1>
+
+	app.setup()
+	app.parse(os.args)
 }
 
-// work の検証をやる
-https://modules.vlang.io/os.html#walk
+fn normalise_paths(paths []string) []string {
+	mut res := paths.map(it.replace(os.path_separator, '/'))
+	res.sort()
+	return res
+}
+
+fn get_paths(path string) []string {
+	mds := os.walk_ext(path, '.md')
+	return mds
+}
