c2bd1636e7829a50bc19dfdb93d2aef3b8503762
zztkm <takumi.basket1682@gmail.com>
2022-07-27 02:17:00 +0900

    🚧

--- vss.v
+++ vss.v
@@ -4,13 +4,15 @@ import os
 import cli
 import markdown
 
-const markdown_text = "
+const markdown_text = '
 # Open Sea
 
 A static site generator
 
 - [GitHub](https://github.com/zztkm)
-"
+'
+
+const default_dist = 'dist'
 
 fn main() {
 	mut app := cli.Command{
@@ -18,22 +20,7 @@ fn main() {
 		version: '0.0.0'
 		description: 'static site generator'
 		execute: fn (cmd cli.Command) ? {
-			paths := get_paths('testfiles')
-			if paths.len == 0 {
-				println('Cloud not retrieve path')
-				return
-			}
-			for path in paths {
-				println(path)
-			}
-
-			// index_html := $embed_file("layouts/_index.html")
-			title := 'tsurutatakumi.info'
-			contents := markdown.to_html(markdown_text)
-
-			index_html := $tmpl('layouts/_index.html')
-			os.write_file('index.html', index_html) ?
-			return
+			generate_index_page()?
 		}
 	}
 
@@ -44,4 +31,29 @@ fn main() {
 fn get_paths(path string) []string {
 	mds := os.walk_ext(path, '.md')
 	return mds
+}
+
+fn generate_index_page() ? {
+	paths := get_paths('testfiles')
+	if paths.len == 0 {
+		println('Cloud not retrieve path')
+		return
+	}
+	for path in paths {
+		println(path)
+	}
+
+	// index_html := $embed_file("layouts/_index.html")
+	title := 'tsurutatakumi.info'
+	contents := markdown.to_html(markdown_text)
+
+	index_html := $tmpl('layouts/_index.html')
+	dist := default_dist
+
+	if !os.exists(dist) {
+		os.mkdir_all(dist, ) // build/_dist/ のようなPATHが渡されても作成できるようにmkdir_allを使う
+	}
+	path := os.join_path(dist, 'index.html')
+	os.write_file(path, index_html)?
+	return
 }
