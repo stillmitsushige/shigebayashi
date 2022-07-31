57785f5a39cfa8fb36c16a4090fc31b55beb4e45
zztkm <takumi.basket1682@gmail.com>
2022-07-31 20:26:30 +0900

    🔖 v0.0.3

--- v.mod
+++ v.mod
@@ -1,7 +1,7 @@
 Module {
 	name: 'vss'
 	description: 'static site generator'
-	version: '0.0.2'
+	version: '0.0.3'
 	license: 'MIT'
 	dependencies: [markdown]
 }
--- vss.v
+++ vss.v
@@ -19,7 +19,7 @@ const default_dist = 'dist'
 fn main() {
 	mut app := cli.Command{
 		name: 'vss'
-		version: '0.0.2'
+		version: '0.0.3'
 		description: 'static site generator'
 		execute: fn (cmd cli.Command) ? {
 			generate_index_page()?
