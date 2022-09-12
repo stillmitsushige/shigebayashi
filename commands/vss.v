bee6801ac2308fe70c9b86b9167eaf6acf0967f7
zztkm <takumi.basket1682@gmail.com>
2022-09-13 04:46:17 +0900

    :sparkles: new feature: serve command

--- commands/vss.v
+++ commands/vss.v
@@ -6,7 +6,7 @@ import cli
 pub fn execute() {
 	mut app := cli.Command{
 		name: 'vss'
-		version: '0.0.9'
+		version: '0.0.10'
 		description: 'static site generator'
 		execute: fn (cmd cli.Command) ? {
 			println(cmd.help_message())
@@ -14,6 +14,7 @@ pub fn execute() {
 	}
 
 	app.add_command(new_build_cmd())
+	app.add_command(new_serve_cmd())
 
 	app.setup()
 	app.parse(os.args)
