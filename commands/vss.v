25ecba3fbb03fdcbfb1ed13d14a7bb01de93d8d7
zztkm <takumi.basket1682@gmail.com>
2022-12-12 15:18:13 +0900

    fix: version

--- commands/vss.v
+++ commands/vss.v
@@ -6,7 +6,7 @@ import cli
 pub fn execute() {
 	mut app := cli.Command{
 		name: 'vss'
-		version: '0.0.12'
+		version: '0.1.0'
 		description: 'static site generator'
 		execute: fn (cmd cli.Command) ! {
 			println(cmd.help_message())
